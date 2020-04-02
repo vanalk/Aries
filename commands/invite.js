6f73fc42d99b1fa5ce439904168ac666193a7a6d
vanalk <45585066+vanalk@users.noreply.github.com>
2020-04-01 22:55:13 -0700

    embed invite cmd

--- commands/invite.js
+++ commands/invite.js
@@ -1,16 +1,25 @@
+const Discord = require('discord.js');
+
 module.exports = {
-	name: 'invite',
+    name: 'invite',
     aliases: ['singleuse'],
     description: 'Make a single use invite',
     args: false,
     guildOnly: true,
-	execute(message, args) {
+    execute(message, args) {
         message.channel.createInvite({ 
             maxUses: 1, 
             temporary: true, 
             unique: true, 
             maxAge: 180 
         })
-            .then(invite => message.channel.send(`Your single use invite is: https://discord.gg/` + invite.code));
-	},
+            .then(invite => {
+                const exampleEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Single use invite')
+                    .setDescription('Your single use invite is: ' + invite.url)
+                
+                message.channel.send(exampleEmbed);
+            });
+    },
 };
